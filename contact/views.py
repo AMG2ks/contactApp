901c176029217323b3a5e67197957bb62d59b779
AMG2ks <gabsiaziz37@gmail.com>
2023-02-06 12:13:28 +0000

    permission classes update

--- contact/views.py
+++ contact/views.py
@@ -3,12 +3,14 @@ from django.views.generic import DeleteView
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, DestroyAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from contact.models import Contact
 from contact.serializers import ContactSerializer, ContactDetailsSerializer
 
 
 # Create your views here.
 class ContactListAPIView(ListAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Contact.objects.all()
     serializer_class = ContactDetailsSerializer
     lookup_field = 'pk'
@@ -22,6 +24,7 @@ class ContactCreateAPIView(CreateAPIView):
 
 
 class ContactDetailsAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Contact.objects.all()
     serializer_class = ContactDetailsSerializer
     lookup_field = 'pk'
--- contactProject/settings.py
+++ contactProject/settings.py
@@ -147,6 +147,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
@@ -179,6 +180,13 @@ DJOSER = {
         'user_delete': 'djoser.serializers.UserDeleteSerializer'
     }
 
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 AUTH_USER_MODEL = 'authentication.User'
